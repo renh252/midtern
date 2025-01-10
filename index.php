fef65dde96f2f9f381a8b4770810067c5eeb257c
renh252 <yeerhh@gmail.com>
2025-01-11 01:17:08 +0800

    把adminlte的檔案進行初始化、加上註解、把sidebar和navbar獨立出來

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 <head>
   <meta charset='utf-8'>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-  <title>AdminLTE v4</title>
+  <title>Guide Page | 寵物認養網站</title>
   <meta name='viewport' content='width=device-width, initial-scale=1'>
 </head>
 <body>
-  <script>window.location.href = './dist/pages/index.html'</script>
+  <!-- 指引頁面，跳轉到指定的首頁 -->
+  <script>window.location.href = './dist/pages/index.php'</script>
 </body>
 </html>
