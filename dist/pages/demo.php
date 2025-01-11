ad1a8a0c10e9407611f022edba23578f6a5342ba
renh252 <yeerhh@gmail.com>
2025-01-12 05:34:07 +0800

    建立demo.php，修改index的padding-top

---
+++ dist/pages/demo.php
@@ -0,0 +1,300 @@
+<?php include __DIR__ . '/parts/head.php' ?>
+<!--begin::Body-->
+<body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
+  <!--begin::App Wrapper 網頁的主要內容在這-->
+  <div class="app-wrapper">
+    <!--begin::Header-->
+    <?php include __DIR__ . '/parts/navbar.php' ?>
+    <!--end::Header-->
+    <!--begin::Sidebar-->
+    <?php include __DIR__ . '/parts/sidebar.php' ?>
+    <!--end::Sidebar-->
+    <!--begin::App Main-->
+    <main class="app-main pt-5">
+      <!--begin::App Content Header-->
+      <div class="app-content-header">
+        <!--begin::Container-->
+        <div class="container-fluid">
+          <!--begin::Row-->
+          <div class="row">
+            <div class="col-sm-6">
+              <h3 class="mb-0">這裡是標題</h3>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-end">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">breadcrumb</li>
+              </ol>
+            </div>
+          </div>
+          <!--end::Row-->
+        </div>
+        <!--end::Container-->
+      </div>
+      <!--end::App Content Header-->
+      <!--begin::App Content-->
+      <div class="app-content">
+        <!--begin::Container-->
+        <div class="container-fluid">
+          這裡是內容
+        </div>
+        <!--end::Container-->
+      </div>
+      <!--end::App Content-->
+    </main>
+    <!--end::App Main-->
+    <!--begin::Footer-->
+    <?php include __DIR__ . '/parts/footer.php' ?>
+    <!--end::Footer-->
+  </div>
+  <!--end::App Wrapper-->
+  <!--begin::Script-->
+  <!--begin::Third Party Plugin(OverlayScrollbars) 可自定義的覆蓋滾動條-->
+  <script
+    src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
+    integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ="
+    crossorigin="anonymous"></script>
+  <!--end::Third Party Plugin(OverlayScrollbars)-->
+  <!--begin::Required Plugin(popperjs for Bootstrap 5) Bootstrap彈出元素"（如工具提示和彈出窗口）-->
+  <script
+    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+  <!--end::Required Plugin(popperjs for Bootstrap 5)-->
+  <!--begin::Required Plugin(Bootstrap 5)-->
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+    crossorigin="anonymous"></script>
+  <!--end::Required Plugin(Bootstrap 5)-->
+  <!--begin::Required Plugin(AdminLTE)-->
+  <script src="../../dist/js/adminlte.js"></script>
+  <!--end::Required Plugin(AdminLTE)-->
+  <!--begin::OverlayScrollbars Configure 設定滾動條-->
+  <script>
+    const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
+    const Default = {
+      // 當鼠標離開滾動區域時，滾動條會自動隱藏；允許用戶通過點擊滾動條來進行滾動
+      scrollbarTheme: 'os-theme-light',
+      scrollbarAutoHide: 'leave',
+      scrollbarClickScroll: true,
+    };
+    // DOMContentLoaded確保在DOM完全加載後執行代碼
+    document.addEventListener('DOMContentLoaded', function() {
+      const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+      if (sidebarWrapper && typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== 'undefined') {
+        // 初始化滾動條，並傳遞配置選項，如主題和自動隱藏行為
+        OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+          scrollbars: {
+            theme: Default.scrollbarTheme,
+            autoHide: Default.scrollbarAutoHide,
+            clickScroll: Default.scrollbarClickScroll,
+          },
+        });
+      }
+    });
+  </script>
+  <!--end::OverlayScrollbars Configure-->
+  <!-- OPTIONAL SCRIPTS 額外功能&實作-->
+  <!-- sortablejs 在網頁上實現可拖放和可排序的元素-->
+  <script
+    src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"
+    integrity="sha256-ipiJrswvAR4VAx/th+6zWsdeYmVae0iJuiR+6OqHJHQ="
+    crossorigin="anonymous"></script>
+  <!-- 用 sortablejs 把.card-header變成可以拖曳的元素-->
+  <script>
+    const connectedSortables = document.querySelectorAll('.connectedSortable');
+    connectedSortables.forEach((connectedSortable) => {
+      let sortable = new Sortable(connectedSortable, {
+        group: 'shared',
+        handle: '.card-header',
+      });
+    });
+
+    const cardHeaders = document.querySelectorAll('.connectedSortable .card-header');
+    cardHeaders.forEach((cardHeader) => {
+      cardHeader.style.cursor = 'move';
+    });
+  </script>
+  <!-- 引入 apexcharts 圖表-->
+  <script
+    src="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.min.js"
+    integrity="sha256-+vh8GkaU7C9/wbSLIcwq82tQ2wTf44aOHA8HlBMwRI8="
+    crossorigin="anonymous"></script>
+  <!-- apexcharts 生成圖表-->
+  <script>
+    // NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+    // 這些都是假資料，用來展示如何生成表格，實際上線需要替換掉
+    // ++++++++++++++++++++++++++++++++++++++++++
+    // 設定圖表選項
+    const sales_chart_options = {
+      series: [{
+          name: 'Digital Goods',
+          data: [28, 48, 40, 19, 86, 27, 90],
+        },
+        {
+          name: 'Electronics',
+          data: [65, 59, 80, 81, 56, 55, 40],
+        },
+      ],
+      chart: {
+        height: 300,
+        type: 'area',
+        toolbar: {
+          show: false,
+        },
+      },
+      legend: {
+        show: false,
+      },
+      colors: ['#0d6efd', '#20c997'],
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: 'smooth',
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: [
+          '2023-01-01',
+          '2023-02-01',
+          '2023-03-01',
+          '2023-04-01',
+          '2023-05-01',
+          '2023-06-01',
+          '2023-07-01',
+        ],
+      },
+      tooltip: {
+        x: {
+          format: 'MMMM yyyy',
+        },
+      },
+    };
+
+    const sales_chart = new ApexCharts(
+      document.querySelector('#revenue-chart'),
+      sales_chart_options,
+    );
+    sales_chart.render();
+  </script>
+  <!-- 引入 jsvectormap 用來嵌入互動式地圖-->
+  <script
+    src="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/js/jsvectormap.min.js"
+    integrity="sha256-/t1nN2956BT869E6H4V1dnt0X5pAQHPytli+1nTZm2Y="
+    crossorigin="anonymous"></script>
+  <script
+    src="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/maps/world.js"
+    integrity="sha256-XPpPaZlU8S/HWf7FZLAncLg2SAkP8ScUTII89x9D3lY="
+    crossorigin="anonymous"></script>
+  <!-- jsvectormap 生成地圖-->
+  <script>
+    // NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+    // 這些都是假資料，用來展示如何生成地圖，實際上線需要替換掉
+    // ++++++++++++++++++++++++++++++++++++++++++
+    // 設定圖表選項
+    const visitorsData = {
+      US: 398, // USA
+      SA: 400, // Saudi Arabia
+      CA: 1000, // Canada
+      DE: 500, // Germany
+      FR: 760, // France
+      CN: 300, // China
+      AU: 700, // Australia
+      BR: 600, // Brazil
+      IN: 800, // India
+      GB: 320, // Great Britain
+      RU: 3000, // Russia
+    };
+
+    // World map by jsVectorMap
+    const map = new jsVectorMap({
+      selector: '#world-map',
+      map: 'world',
+    });
+
+    // Sparkline charts
+    const option_sparkline1 = {
+      series: [{
+        data: [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021],
+      }, ],
+      chart: {
+        type: 'area',
+        height: 50,
+        sparkline: {
+          enabled: true,
+        },
+      },
+      stroke: {
+        curve: 'straight',
+      },
+      fill: {
+        opacity: 0.3,
+      },
+      yaxis: {
+        min: 0,
+      },
+      colors: ['#DCE6EC'],
+    };
+
+    const sparkline1 = new ApexCharts(document.querySelector('#sparkline-1'), option_sparkline1);
+    sparkline1.render();
+
+    const option_sparkline2 = {
+      series: [{
+        data: [515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921],
+      }, ],
+      chart: {
+        type: 'area',
+        height: 50,
+        sparkline: {
+          enabled: true,
+        },
+      },
+      stroke: {
+        curve: 'straight',
+      },
+      fill: {
+        opacity: 0.3,
+      },
+      yaxis: {
+        min: 0,
+      },
+      colors: ['#DCE6EC'],
+    };
+
+    const sparkline2 = new ApexCharts(document.querySelector('#sparkline-2'), option_sparkline2);
+    sparkline2.render();
+
+    const option_sparkline3 = {
+      series: [{
+        data: [15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21],
+      }, ],
+      chart: {
+        type: 'area',
+        height: 50,
+        sparkline: {
+          enabled: true,
+        },
+      },
+      stroke: {
+        curve: 'straight',
+      },
+      fill: {
+        opacity: 0.3,
+      },
+      yaxis: {
+        min: 0,
+      },
+      colors: ['#DCE6EC'],
+    };
+
+    const sparkline3 = new ApexCharts(document.querySelector('#sparkline-3'), option_sparkline3);
+    sparkline3.render();
+  </script>
+  <!--end::Script-->
+</body>
+<!--end::Body-->
+
+</html>
