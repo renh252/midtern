7991a7f85cbbfe5eccca979c3eb9dd443f38bfbc
renh252 <yeerhh@gmail.com>
2025-01-12 05:26:44 +0800

    建立demo.php，修改index的padding-top

--- dist/pages/demo.php
+++ dist/pages/demo.php
@@ -23,7 +23,7 @@
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-end">
                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                <li class="breadcrumb-item active" aria-current="page">某某頁面</li>
               </ol>
             </div>
           </div>
