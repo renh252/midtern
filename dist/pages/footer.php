3971abcd311c0509dea20d8a108ab4385329695a
renh252 <yeerhh@gmail.com>
2025-01-11 21:49:41 +0800

    新增說明用txt檔、把footer獨立出來

---
+++ dist/pages/footer.php
@@ -0,0 +1,12 @@
+<footer class="app-footer">
+  <!--begin::Copyright-->
+  <strong>
+    Copyright &copy; 2014-2024&nbsp;
+    <a href="index.php" class="text-decoration-none">寵物認養網站</a>
+  </strong>
+  All rights reserved.
+  <!--end::Copyright-->
+  <!--begin::To the end-->
+  <div class="float-end d-none d-sm-inline">這裡放其他功能性按鈕</div>
+  <!--end::To the end-->
+</footer>
