ade031a9d6eae98a14caff0267424968cfd26616
Ren Huang <yeerhh@gmail.com>
2025-01-13 11:30:24 +0800

    新增init.php和config.php

---
+++ dist/pages/parts/config.php
@@ -0,0 +1,12 @@
+<?php
+# address-book/parts/config.php 設定檔，不加入版本控制
+# 資料庫連線設定
+const DB_HOST = '127.0.0.1';
+const DB_USER = 'root';
+const DB_PASS = 'P@ssw0rd';
+const DB_NAME = 'pet_adopt';
+const DB_PORT = 3306; #若不是使用 3306 需要設定
+
+
+# 設定網站常數，你的專案資料夾路徑
+const PATH_BASE = '/midtern';
---
+++ dist/pages/parts/init.php
@@ -0,0 +1,7 @@
+<?php 
+if (!isset($_SESSION)) {
+    // 如果尚未啟動 session 的功能, 就啟動
+   session_start();
+}
+require __DIR__. '/db-connect.php'; 
+?>
