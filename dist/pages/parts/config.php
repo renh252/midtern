02d69173335a539079a7114ed791145a178bcfa1
Ren Huang <yeerhh@gmail.com>
2025-01-13 14:59:23 +0800

    修改init和config，建立相對根目錄的路徑常數

--- dist/pages/parts/config.php
+++ dist/pages/parts/config.php
@@ -8,12 +8,3 @@ const DB_NAME = 'pet_adopt';
 const DB_PORT = 3306; #若不是使用 3306 需要設定
 
 
-// 定義網站根目錄的 URL（用於 href 連結）
-define('ROOT_URL', '/midtern/'); 
-
-// 網站根目錄的系統路徑（用於 PHP 內文中）
-define('ROOT_PATH', dirname(__DIR__, 3) . '/');
-// __DIR__ 指向 config.php 所在目錄，dirname(__DIR__, 3) 往上三層到根目錄
-
-
-// include ROOT_PATH . 'pages/parts/navbar.php'; // 使用ROOT_PATH引入 navbar.php
