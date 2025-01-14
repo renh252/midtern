4b52293869abd719e64b474d0ba7891c390966bc
Ren Huang <yeerhh@gmail.com>
2025-01-14 10:10:39 +0800

    修正init的註解說明

--- dist/pages/parts/init.php
+++ dist/pages/parts/init.php
@@ -5,19 +5,22 @@ if (!isset($_SESSION)) {
 }
 require __DIR__. '/db-connect.php'; 
 
-// 定義根目錄的 URL（用於 href 連結）
-define('ROOT_URL', '/midtern/'); 
+
 
 // 根目錄的系統路徑（用於 PHP 內文中）
 define('ROOT_PATH', dirname(__DIR__, 3) . '/');
 // __DIR__ 指向 config.php 所在目錄，dirname(__DIR__, 3) 往上三層到根目錄
+/* 
+使用ROOT_PATH引入 navbar.php
+include ROOT_PATH . 'pages/parts/navbar.php';
+*/
 
-
-// include ROOT_PATH . 'pages/parts/navbar.php'; // 使用ROOT_PATH引入 navbar.php
-
-/* 使用 ROOT_URL 來建立連結
-<a href="<?= ROOT_URL ?>index.php">首頁</a>
-<a href="<?= ROOT_URL ?>pages/pets/pet_info.php">寵物資訊</a>
-<img src="<?= ROOT_URL ?>images/logo.png" alt="Logo">
+// 定義根目錄的 URL（用於 href、src 連結等）
+define('ROOT_URL', '/midtern/'); 
+/* 
+使用 ROOT_URL 來建立連結
+<a href="<?= ROOT_URL ?>/dist/pages/index.php">到首頁</a>
+<a href="<?= ROOT_URL ?>/dist/pages/pets/pet_info.php">到寵物資訊</a>
+<script src="<?=ROOT_URL?>dist/js/adminlte.js"></script>
 */
 ?>
