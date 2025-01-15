80b49a87a2adca4be9c3dd0db0e2e2d7e063e690
Ren Huang <yeerhh@gmail.com>
2025-01-15 16:40:36 +0800

    更改config.php

--- dist/pages/shop/myProject/parts/config.php
+++ dist/pages/shop/myProject/parts/config.php
@@ -3,11 +3,11 @@
 
 // 資料庫連線設定
 const DB_HOST = "127.0.0.1";  //資料庫伺服器的位址
-const DB_USER = "shiao_ching"; //資料庫的使用者名稱
+const DB_USER = "root"; //資料庫的使用者名稱
 const DB_PASS = "P@ssw0rd"; //資料庫的使用者密碼
-const DB_NAME = "shopping_store";  //資料庫名稱
+const DB_NAME = "pet_proj";  //資料庫名稱
 const DB_PORT = "3306"; // MySQL 的通訊埠
 
 // 設定網站常數，你的專案資料夾路徑
 //定義靜態路徑基底（base path），方便在專案中使用相對路徑
-const PATH_BASE ='myProject';
+const PATH_BASE ='/midtern/';
