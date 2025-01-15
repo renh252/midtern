084fdbeaef452507e1517a7e663e3c85fb35373a
renh252 <yeerhh@gmail.com>
2025-01-15 22:38:00 +0800

    修改navbar sidebar以及登出按鈕、和一些為了登入驗證而新增的東西

--- dist/pages/parts/sidebar.php
+++ dist/pages/parts/sidebar.php
@@ -120,7 +120,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= ROOT_URL ?>dist/pages/forums/blacklist.php" class="nav-link">
+          <a href="<?= ROOT_URL ?>dist/pages/forums/user_status.php" class="nav-link">
               <i class="nav-icon bi bi-circle"></i>
               <p>黑名單</p>
             </a>
