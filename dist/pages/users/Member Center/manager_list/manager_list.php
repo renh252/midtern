81988b12a4984ac616a564f6c79e3f0c37f615b1
Weng <4a7h0075@gmail.com>
2025-01-16 12:28:24 +0800

    edit title

--- dist/pages/users/Member Center/manager_list/manager_list.php
+++ dist/pages/users/Member Center/manager_list/manager_list.php
@@ -129,9 +129,6 @@ if (!isset($_SESSION['login_session']) || $_SESSION['login_session'] !== true) {
                     <!--begin::Row-->
                     <div class="row">
                         <div class="col-sm-6">
-                            <h3 class="mb-0">管理員列表</h3>
-                        </div>
-                        <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-end">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">breadcrumb</li>
