fe6650b8bd90c6c4d2d4cb50c1a2e5e8029a8bf6
Ren Huang <yeerhh@gmail.com>
2025-01-16 10:42:42 +0800

    sidebar改回，推送新的database

--- dist/pages/parts/sidebar.php
+++ dist/pages/parts/sidebar.php
@@ -80,7 +80,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?= ROOT_URL ?>dist/pages/shop/return.php" class="nav-link">
+            <a href="<?= ROOT_URL ?>dist/pages/shop/return.php" class="nav-link" onclick="return checkPrivilege('shop', this)">
               <i class="nav-icon bi bi-circle"></i>
               <p>商品類別</p>
             </a>
